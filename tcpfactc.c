e523acf2c4cd85084fe2b12b9dbba1626504cdf9
Anjana V P <anjanavp173@gmail.com>
2025-04-07 21:09:11 +0530

    Add files via upload

---
+++ tcpfactc.c
@@ -0,0 +1,54 @@
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+int main() {
+    int port, sock_fd;
+    char msg[512], message[20];
+    struct sockaddr_in client_addr;
+
+    printf("Enter Port: ");
+    scanf("%d", &port);
+
+    sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock_fd < 0) {
+        perror("Error creating socket");
+        exit(1);
+    }
+    printf("Socket Created.\n");
+
+    client_addr.sin_family = AF_INET;
+    client_addr.sin_port = htons(port);
+    client_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    if (connect(sock_fd, (struct sockaddr *)&client_addr, sizeof(client_addr)) < 0) {
+        perror("Error connecting");
+        close(sock_fd);
+        exit(1);
+    }
+    printf("Connected to server.\n\n");
+
+    while (1) {
+        memset(message, 0, sizeof(message));
+        printf("Enter a number (or 'quit' to exit): "); 
+        scanf("%s", message);
+
+        send(sock_fd, message, strlen(message), 0);
+
+        if (strcmp(message, "quit") == 0) {
+            break;
+        }
+
+        memset(msg, 0, sizeof(msg));
+        recv(sock_fd, msg, sizeof(msg), 0);
+        printf("Factorial:%s\n", msg);
+    }
+
+    close(sock_fd);
+    printf("\nConnection closed.\n");
+    return 0;
+}
---
+++ tcpfibs.c
@@ -0,0 +1,80 @@
+// server.c
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+int main() 
+{
+    int sock_fd, newsockfd;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t client_len;
+    char buffer[256], response[512];
+
+    sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock_fd < 0) {
+        perror("Error creating socket");
+        exit(1);
+    }
+    printf("Socket created\n");
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(8080);
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+
+    if (bind(sock_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Error binding");
+        close(sock_fd);
+        exit(1);
+    }
+    printf("Binded\n");
+
+    if (listen(sock_fd, 5) < 0) {
+        perror("Error listening");
+        close(sock_fd);
+        exit(1);
+    }
+    printf("Listening...\n");
+
+    client_len = sizeof(client_addr);
+    newsockfd = accept(sock_fd, (struct sockaddr *)&client_addr, &client_len);
+    if (newsockfd < 0) {
+        perror("Error accepting connection");
+        close(sock_fd);
+        exit(1);
+    }
+    printf("Client connected\n\n");
+
+    while (1) {
+        memset(buffer, 0, sizeof(buffer));
+        recv(newsockfd, buffer, sizeof(buffer), 0);
+
+        if (strcmp(buffer, "quit") == 0) {
+            break;
+        }
+
+        int n = atoi(buffer);
+        int a = 0, b = 1;
+        char temp[20];
+        memset(response, 0, sizeof(response));
+
+        for (int i = 0; i < n; i++) {
+            sprintf(temp, "%d ", a);
+            strcat(response, temp);
+            int next = a + b;
+            a = b;
+            b = next;
+        }
+
+        send(newsockfd, response, strlen(response), 0);
+    }
+
+    close(newsockfd);
+    close(sock_fd);
+    printf("\nConnection closed.\n");
+    return 0;
+}
